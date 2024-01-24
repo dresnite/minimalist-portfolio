2bea69ea6b9ac24e1ecd3aa98167960818c24908
andresbytes <giantquartz@gmail.com>
2024-01-24 02:48:41 +0100

    Rename hire me button to contact me

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
                 <p>Life&apos;s too short to take it slow, so I stay building fast.</p>
 
                 <div className="pt-2 flex flex-col space-y-4 md:space-y-0 md:flex-row md:space-x-10">
-                    <button className="border-2 border-black bg-black text-white py-2 w-44 hover:font-bold">Hire me</button>
+                    <button className="border-2 border-black bg-black text-white py-2 w-44 hover:font-bold">Contact me</button>
                     <button className="border-2 border-black py-2 w-44 hover:font-bold">Browse projects</button>
                 </div>
             </div>
