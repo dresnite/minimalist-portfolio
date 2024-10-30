ef3838c02d8cbe1664351f649f247c63c971a0ad
Andrés Arias <giantquartz@gmail.com>
2024-10-30 14:49:39 +0100

    age++

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -13,7 +13,7 @@ export default function AboutMe() {
         <div className="px-10 max-w-3/4 md:max-w-100 md:ml-28  md:top-48 md:left-80 bg-white md:absolute">
           <div className="mt-16 mb-12 max-w-[45rem] mx-auto flex flex-col gap-3">
             <p>
-              I&apos;m Andrés. A 22-year old software developer, maker and problem
+              I&apos;m Andrés. A 23-year old software developer, maker and problem
               solver.
             </p>
 
